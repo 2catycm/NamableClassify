120ca9af25a575ac9e4d22d979a9d2730f0ba0b7
叶璨铭 <12011404@mail.sustech.edu.cn>
2024-10-16 02:58:52 +0800

    feat: main module

--- namable_classify/core.py
+++ namable_classify/core.py
@@ -9,7 +9,7 @@ __all__ = ['ClassificationModelConfig', 'HuggingfaceModel', 'ClassificationTaskC
 import os
 os.environ['HF_ENDPOINT'] = "https://hf-mirror.com" # TODO this is optional for Foreigners
 
-# %% ../nbs/00_core.ipynb 6
+# %% ../nbs/00_core.ipynb 7
 from pydantic import BaseModel
 class ClassificationModelConfig(BaseModel):
     provider: str = "huggingface"
@@ -31,6 +31,7 @@ class HuggingfaceModel(nn.Module):
         # self.image_preprocessor = BitImageProcessor.from_pretrained(config.model_checkpoint, use_fast=True)
         self.image_preprocessor = AutoImageProcessor.from_pretrained(config.checkpoint)
         self.backbone: ViTModel = AutoModel.from_pretrained(config.checkpoint) # TODO we now just consider ViTModel
+        self.backbone.train()
         self.backbone_config: ViTConfig = self.backbone.config # 包括了 image_size 和 hidden_size 这两个重要信息
         if config.head_strategy == "linear":
             self.head = nn.Linear(self.backbone_config.hidden_size, config.num_of_classes)
@@ -58,7 +59,7 @@ def get_cls_model(self:ClassificationModelConfig):
     else:
         raise NotImplementedError("Only huggingface is supported for now. ")
 
-# %% ../nbs/00_core.ipynb 7
+# %% ../nbs/00_core.ipynb 8
 import lightning as L
 from pydantic import BaseModel
 from .data import ClassificationDataConfig, ClassificationDataModule
@@ -70,7 +71,7 @@ class ClassificationTaskConfig(BaseModel):
     dataset_config: ClassificationDataConfig = ClassificationDataConfig()
     learning_rate: float = 3e-4
 
-# %% ../nbs/00_core.ipynb 8
+# %% ../nbs/00_core.ipynb 9
 import lightning as L
 from lightning.pytorch.utilities.types import EVAL_DATALOADERS, TRAIN_DATALOADERS, STEP_OUTPUT, OptimizerLRScheduler
 from overrides import override
@@ -79,6 +80,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from .utils import print_model_pretty
+from .utils import partial_with_self, append_dict_list, compute_classification_metrics, ensure_array, logger
+import numpy as np
+from typing import Any
 class ClassificationTask(L.LightningModule):
     def __init__(self, config: ClassificationTaskConfig)->None:
         super().__init__()
@@ -95,12 +99,17 @@ class ClassificationTask(L.LightningModule):
         # 现在需要更新数据
         self.lit_data.set_transform_from_hf_image_preprocessor(hf_image_preprocessor=self.cls_model.image_preprocessor)
         
+        model_image_size:tuple[int, int] = (self.cls_model.image_preprocessor.size['height'], self.cls_model.image_preprocessor.size['width'])
+        self.example_input_array = torch.Tensor(1, self.cls_model.backbone.config.num_channels, *model_image_size)
         
         # 最后是训练策略
-        self.loss = nn.CrossEntropyLoss(label_smoothing=config.label_smoothing)
         self.softmax = nn.Softmax(dim=1)    
+        self.loss = nn.CrossEntropyLoss(label_smoothing=config.label_smoothing)
         # nn.LogSoftmax(dim=1)
         # https://blog.csdn.net/qq_43391414/article/details/118421352 logsoftmax+nll的速度快，但是没有label smoothing
+        
+        # 评价策略
+        self.evaluation_steps_outputs = dict()
     
     def compute_model_logits(self, image_tensor:torch.Tensor)-> torch.Tensor:
         return self.cls_model(image_tensor)
@@ -125,5 +134,55 @@ class ClassificationTask(L.LightningModule):
         return torch.optim.AdamW(self.parameters(), lr=self.hparams.learning_rate)
         # return L.AdamW(self.parameters(), lr=self.learning_rate)
 
+    # 现在我们已经定义好Training的逻辑了，已经可以跑训练了。然而，除了训练之外，我们需要评测模型的性能。
     # @override
     # def 
+    def on_evaluation_epoch_start(self, stage:str=""):
+        self.evaluation_steps_outputs = dict()
+        self.evaluation_steps_outputs[f'{stage}_batch_probs'] = []
+        self.evaluation_steps_outputs[f'{stage}_label_tensor'] = []
+            
+    def evaluation_step(self, batch, batch_idx=None, stage:str="", *args: Any, **kwargs: Any) -> STEP_OUTPUT:
+        image_tensor, label_tensor = batch
+        batch_probs = self(image_tensor)
+        append_dict_list(self.evaluation_steps_outputs, f'{stage}_batch_probs', ensure_array(batch_probs))
+        append_dict_list(self.evaluation_steps_outputs, f'{stage}_label_tensor', ensure_array(label_tensor))
+        batch_loss = self.loss(batch_probs, label_tensor)
+        self.log(f"{stage}_loss", batch_loss, prog_bar=True)
+        return batch_loss
+            
+    def on_evaluation_epoch_end(self, stage:str=""):
+        # https://github.com/Lightning-AI/pytorch-lightning/discussions/9845
+        # labels = self.lit_data.classes
+        labels = list(range(self.lit_data.num_of_classes))
+        # labels = None
+        # print(labels)
+        # stack 是 new axis， concat是existing axis
+        all_pred_probs = np.concatenate(self.evaluation_steps_outputs[f'{stage}_batch_probs'])
+        all_label_tensor = np.concatenate(self.evaluation_steps_outputs[f'{stage}_label_tensor'])
+        # logger.debug(self.evaluation_steps_outputs[f'{stage}_label_tensor'])
+        # logger.debug(all_label_tensor)
+        eval_dict = compute_classification_metrics(all_label_tensor, all_pred_probs, logits_to_prob=False, 
+                                                labels=labels)
+        eval_dict = {f"{stage}_{k}": v for k,v in eval_dict.items()}
+        self.log_dict(eval_dict)
+        self.evaluation_steps_outputs.clear()
+
+    def on_validation_epoch_start(self):
+        return self.on_evaluation_epoch_start(stage="val")
+
+    def on_test_epoch_start(self):
+        return self.on_evaluation_epoch_start(stage="test")
+
+    def on_validation_epoch_end(self):
+        return self.on_evaluation_epoch_end(stage="val")
+
+    def on_test_epoch_end(self):
+        return self.on_evaluation_epoch_end(stage="test")
+
+    def validation_step(self, batch, batch_idx=None):
+        return self.evaluation_step(batch, batch_idx, stage="val")
+
+    def test_step(self, batch, batch_idx=None):
+        return self.evaluation_step(batch, batch_idx, stage="test")
+
